98973616d88dc8d3e43c475e1b0208f248a1a069
Jose <joavila@unal.edu.co>
2023-10-07 18:41:07 -0500

    Added CSS normalize

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import App from "./App.tsx";
+import "./normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
