a11389a0f3fc5ad9657a2121d4fe70c59868bb4a
edgardanielgd <edgardanielgd123@gmail.com>
2023-11-10 09:17:16 -0500

    Refactorized to create multiple images versions

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config: any = {
     //TARGET_GATEWAY: import.meta.env.VITE_TARGET_GATEWAY,
-    TARGET_GATEWAY: 'https://habitus-gateway-ik25vlw3ta-rj.a.run.app'
+    TARGET_GATEWAY: 'http://10.158.0.7:4000'
 }
